61d7dbcd018c058808633f3de7958591acefee17
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-04-25 17:56:27 +0200

    bind correctly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 gnunet-arm -s > $HOME/gnunet.log 2>&1
+gnunet-config -s rest -o BIND_TO -V "0.0.0.0"
+gnunet-config -s rest -o BIND_TO6 -V "::"
 gnunet-arm -i reclaim
+gnunet-arm -i rest
+
 exec bash
